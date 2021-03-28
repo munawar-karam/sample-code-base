0ea73af2fa97a8a3a73fda23bb80e24967d139ed
Fahad Qamar <fahad.uol.ce@gmail.com>
2021-03-28 14:39:25 +0500

    reset password link and update reset password completed

--- routes/api.php
+++ routes/api.php
@@ -17,6 +17,7 @@ Route::post('/signup','App\Http\Controllers\SignupController@user_signup');
 Route::post('/login','App\Http\Controllers\LoginController@user_login');
 Route::post('/forget_password','App\Http\Controllers\PasswordResetController@generate_link');
 Route::get('/reset_password/{token}','App\Http\Controllers\PasswordResetController@reset_password');
+Route::post('/update_reset_password','App\Http\Controllers\PasswordResetController@update_password');
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/logout', 'App\Http\Controllers\LogoutController@user_logout');
